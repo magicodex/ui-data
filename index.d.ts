14ecf215c34c0207625b58b0aba118f113a4837c
magicodex <zhuangmaohui@163.com>
2023-08-11 22:24:04 +0800

    发布v1.3.6

--- index.d.ts
+++ index.d.ts
@@ -11,6 +11,12 @@ export class Model {
 
   getBaseElement(): any;
 
+  /**
+   * 获取指定表达式对应元素的数据
+   * @param {(string|string[])} expression 表达式
+   * @param {function} [skipFn] 判断是否跳过值,比如 (targetValue) => (targetValue == null)
+   * @returns {*} 值
+   */
   getData(expression: any, skipFn: any): any;
 
   getDataHandlerByElement(element: any): any;
@@ -19,44 +25,56 @@ export class Model {
 
   queryElementsBySelector(selector: any): any;
 
+  /**
+   * 设置指定表达式对应元素的数据
+   * @param {string|string[]} expression 表达式
+   * @param {*} value 值
+   * @param {boolean} [notSkipSetIfValueAbsent=false] 是否跳过没有指定值的元素,默认 false 跳过没有指定值的元素
+   */
   setData(expression: any, value: any, notSkipSetIfValueAbsent: any): void;
 
   static config: {
-      defaultNameAttributeName: string;
-      defaultTypeAttributeName: string;
-      initDataHandlers: {
-          "checkbox-boolean": {
-              getValue: any;
-              setValue: any;
-          };
-          default: {
-              getInputElementValue: any;
-              getSelectElementValue: any;
-              getValue: any;
-              setInputElementValue: any;
-              setSelectElementValue: any;
-              setValue: any;
-          };
+    defaultNameAttributeName: string;
+    defaultTypeAttributeName: string;
+    initDataHandlers: {
+      "checkbox-boolean": {
+        getValue: any;
+        setValue: any;
       };
+      default: {
+        getInputElementValue: any;
+        getSelectElementValue: any;
+        getValue: any;
+        setInputElementValue: any;
+        setSelectElementValue: any;
+        setValue: any;
+      };
+    };
   };
 
 }
 
 export const dataHandlers: {
   checkboxBooleanHandler: {
-      getValue: any;
-      setValue: any;
+    getValue: any;
+    setValue: any;
   };
   defaultDataHandler: {
-      getInputElementValue: any;
-      getSelectElementValue: any;
-      getValue: any;
-      setInputElementValue: any;
-      setSelectElementValue: any;
-      setValue: any;
+    getInputElementValue: any;
+    getSelectElementValue: any;
+    getValue: any;
+    setInputElementValue: any;
+    setSelectElementValue: any;
+    setValue: any;
   };
 };
 
+/**
+ * 创建数据模型对象并返回
+ * @param {(Document|Element)} baseElement
+ * @param {object} [opts]
+ * @returns {Model}
+ */
 export function model(baseElement: any, opts: any): Model;
 
 export namespace utils {
